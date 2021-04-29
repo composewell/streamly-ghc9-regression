e45436297c328dd26ec74756606e26cd5af11cfa
Harendra Kumar <harendra@composewell.com>
2021-04-29 20:46:29 +0530

    Keeping ops in the same module makes the issue disappear

--- Main.hs
+++ Main.hs
@@ -1,8 +1,27 @@
 module Main (main) where
 
 import StreamK (IsStream, MonadAsync)
-import Operations (unfoldrM, drain)
+-- import Operations (unfoldrM, drain)
 import qualified StreamK
+import StreamK (IsStream, MonadAsync)
+import qualified StreamK as K
+import qualified StreamD as D
+import qualified Serial
+
+{-# INLINE [2] unfoldrM #-}
+unfoldrM :: (IsStream t, MonadAsync m) => (b -> m (Maybe (a, b))) -> b -> t m a
+unfoldrM = K.unfoldrM
+
+{-# RULES "unfoldrM serial" unfoldrM = unfoldrMSerial #-}
+{-# INLINE [2] unfoldrMSerial #-}
+unfoldrMSerial :: MonadAsync m => (b -> m (Maybe (a, b))) -> b -> K.Stream m a
+unfoldrMSerial = Serial.unfoldrM
+
+{-# INLINE [2] drain #-}
+drain :: (IsStream t, Monad m) => t m a -> m ()
+drain m = D.drain $ D.fromStreamK (K.toStream m)
+{-# RULES "drain fallback to CPS" [1]
+    forall a. D.drain (D.fromStreamK a) = K.drain a #-}
 
 {-# INLINE sourceUnfoldrM #-}
 sourceUnfoldrM :: MonadAsync m => StreamK.Stream m Int
